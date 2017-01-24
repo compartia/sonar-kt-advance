83bac7982cf637b075ef3220aad11b092c064404
Artem Zaborskii <compartia@gmail.com>
2017-01-24 11:59:38 +0800

    :whale: moving docker files to the separate repo https://github.com/kestreltechnology/docker-sonar-kt-advance

--- docker/docker-entrypoint-initdb.d/init-sonar-db.sh
+++
@@ -1,3 +0,0 @@
-echo "*** restoring sonar db ***"
-set -e
-pg_restore -d sonar -U sonar docker-entrypoint-initdb.d/sonar.dump
--- docker/run.sh
+++
@@ -1,2 +0,0 @@
-docker build -t kestreltechnology/kt-sonarqube-plugin:5.5.6 .
-docker-compose up
